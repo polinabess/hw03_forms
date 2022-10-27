7dd289990598953a6ba24711e5ac79434a2c8f9e
Polina Bessogonova <girls_kiss@mail.ru>
2022-10-27 04:25:04 +0300

    исправление ошибок

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('profile/<str:username>/', views.profile, name='profile'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
-    path('posts/create/', views.post_create, name='post_create'),
+    path('/create/', views.post_create, name='post_create'),
 ]
